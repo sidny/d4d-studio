3f0418af6b00cea03a9b32445e458495d090ec33
duo.fan.yang <duo.fan.yang@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-10-05 07:08:53 +0000

    add D4D

---
+++ D4D.Application/Model/News.cs
@@ -0,0 +1,88 @@
+﻿using System;
+namespace D4D.Model
+{
+	/// <summary>
+	/// 实体类News 。(属性说明自动提取数据库字段的描述信息)
+	/// </summary>
+	public class News
+	{
+		public News()
+		{}
+		#region Model
+		private int _id;
+		private string _title;
+		private string _detail;
+		private string _preview;
+		private DateTime _adddate;
+		private int? _newtype;
+		private bool _publish;
+		private int _hits;
+		/// <summary>
+		/// 
+		/// </summary>
+		public int Id
+		{
+			set{ _id=value;}
+			get{return _id;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Title
+		{
+			set{ _title=value;}
+			get{return _title;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Detail
+		{
+			set{ _detail=value;}
+			get{return _detail;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Preview
+		{
+			set{ _preview=value;}
+			get{return _preview;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public DateTime AddDate
+		{
+			set{ _adddate=value;}
+			get{return _adddate;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public int? NewType
+		{
+			set{ _newtype=value;}
+			get{return _newtype;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public bool Publish
+		{
+			set{ _publish=value;}
+			get{return _publish;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public int Hits
+		{
+			set{ _hits=value;}
+			get{return _hits;}
+		}
+		#endregion Model
+
+	}
+}
+
