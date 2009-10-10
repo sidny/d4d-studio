7fe1f385ac811883c655615e58b8425aee69168e
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-10-10 12:51:01 +0000

    add d4d.platform

---
+++ branches/D4D/D4D.Platform/Providers/TagsProvider.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using D4D.Platform.Domain;
+using D4D.Platform.Persistence;
+namespace D4D.Platform.Providers
+{
+    public class TagsProvider
+    {
+        #region instance
+        private static readonly TagsProvider instance = new TagsProvider();
+
+        internal static TagsProvider Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        #endregion
+        /// <summary>
+        /// tagid<0 表示新增
+        /// >0 则为更新
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <returns></returns>
+        public int SetTag(Tag tag)
+        {
+            return TagsDao.SetTag(tag);
+        }
+        /// <summary>
+        /// 删除tag
+        /// </summary>
+        /// <param name="tagId"></param>
+        public void DeleteTag(int tagId)
+        {
+            TagsDao.DeleteTag(tagId);
+        }
+        /// <summary>
+        /// tag 计数+1
+        /// </summary>
+        /// <param name="tagId"></param>
+        public void AddTagHit(int tagId)
+        {
+            TagsDao.AddTagHit(tagId);
+        }
+        /// <summary>
+        /// 获取OneTag
+        /// </summary>
+        /// <param name="tagId"></param>
+        /// <returns></returns>
+        public Tag GetTag(int tagId)
+        {
+            return TagsDao.GetTag(tagId);
+        }
+        /// <summary>
+        /// 获取tag的top列表
+        /// </summary>
+        /// <param name="maxCount"></param>
+        /// <returns></returns>
+        public List<Tag> GetTopTag(int maxCount)
+        {
+            return TagsDao.GetTopTag(maxCount);
+        }
+    }
+}
