4eb58f15974580a636b5a1cffd37a2069d509a22
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-10-10 12:51:01 +0000

    add d4d.platform

---
+++ D4D.Platform/Domain/Tag.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace D4D.Platform.Domain
+{
+    public class Tag
+    {
+
+        public Tag() { }
+        public Tag(int tagId)
+        {
+            TagId = tagId;
+        }
+        public int TagId
+        {
+            get;
+            set;
+        }
+        public string TagName
+        {
+            get;
+            set;
+        }
+        public int Hits
+        {
+            get;
+            set;
+        }
+        public int AddUserID
+        {
+            get;
+            set;
+        }
+        public DateTime AddDate
+        {
+            get;
+            set;
+        }
+    }
+}
