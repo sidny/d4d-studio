5bba36d74cd42bd37b5a40592decf8dd757163aa
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-10-13 14:01:09 +0000

    add UserLogin

---
+++ branches/D4D/D4D.Platform/Domain/UserLogin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace D4D.Platform.Domain
+{
+    public class UserLogin
+    {
+        public int UserId
+        {
+            get;
+            set;
+        }
+
+        public string UserName
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public string Email
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ branches/D4D/D4D.Platform/Providers/UserProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using D4D.Platform.Domain;
+using D4D.Platform.Persistence;
+namespace D4D.Platform.Providers
+{
+    public class UserProvider
+    {
+        #region instance
+        private static readonly UserProvider instance = new UserProvider();
+
+        internal static UserProvider Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        #endregion
+
+        public void SetUserLogin(UserLogin u)
+        {
+            UserDao.SetUserLogin(u);
+        }
+
+        public void DeleteUserLogin(int userId)
+        {
+            UserDao.DeleteUserLogin(userId);
+        }
+
+        public UserLogin GetUserLoginFromEmail(string email)
+        {
+            return UserDao.GetUserLoginFromEmail(email);
+        }
+    }
+}
