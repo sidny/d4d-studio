c010d4d2b9965e46b31d5b40b4379a31bfd7c459
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-10-14 08:28:57 +0000

    add bandInfo

---
+++ branches/D4D/D4D.Platform/Domain/BandInfo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace D4D.Platform.Domain
+{
+    public class BandInfo
+    {
+        public int BandId
+        {
+            get;
+            set;
+        }
+
+        public string BandName
+        {
+            get;
+            set;
+        }
+
+        public string Info1
+        {
+            get;
+            set;
+        }
+
+        public string Info2
+        {
+            get;
+            set;
+        }
+
+        public string Info3
+        {
+            get;
+            set;
+        }
+
+
+        public string Remark
+        {
+            get;
+            set;
+        }
+
+    }
+}
