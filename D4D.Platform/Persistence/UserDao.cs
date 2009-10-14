36fca36e125947c9d2f2c9d3e9e2fe21ebf5ff9d
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-10-14 08:28:57 +0000

    add bandInfo

--- D4D.Platform/Persistence/UserDao.cs
+++ D4D.Platform/Persistence/UserDao.cs
@@ -8,6 +8,7 @@ namespace D4D.Platform.Persistence
 {
     internal static class UserDao
     {
+        #region UserLogin
         internal static void SetUserLogin(UserLogin u)
         {
             if (u == null) return;
@@ -50,6 +51,67 @@ namespace D4D.Platform.Persistence
             }
             return u;
         }
+        #endregion
+
+        #region BandInfo
+        internal static int SetBandInfo(BandInfo info)
+        {
+            if (info == null) return -1;
+
+            SafeProcedure.ExecuteNonQuery(
+             Database.GetDatabase(D4DDefine.DBInstanceName),
+             "dbo.BandInfo_Set",
+             delegate(IParameterSet parameters)
+             {
+                 parameters.AddWithValue("@BandId", info.BandId);
+                 parameters.AddWithValue("@BandName", info.BandName);
+                 parameters.AddWithValue("@Info1", info.Info1);
+                 parameters.AddWithValue("@Info2", info.Info2);
+                 parameters.AddWithValue("@Info3", info.Info3);
+                 parameters.AddWithValue("@Remark", info.Remark);                
+                 parameters.AddWithValue("@RetVal", DBNull.Value, ParameterDirectionWrap.ReturnValue);
+
+             },
+             delegate(IParameterSet outputParameters)
+             {
+                 info.BandId = Convert.ToInt32(outputParameters.GetValue("@RetVal"));
+             });
+
+            return info.BandId;
+        }
+
+        internal static void MapBandInfoList(IRecord record, List<BandInfo> list)
+        {
+            BandInfo m = new BandInfo();
+            m.BandId = record.GetInt32OrDefault(0, 0);
+            m.BandName = record.GetStringOrEmpty(1);
+            m.Info1 = record.GetStringOrEmpty(2);
+            m.Info2 = record.GetStringOrEmpty(3);
+            m.Info3 = record.GetStringOrEmpty(4);
+            m.Remark = record.GetStringOrEmpty(5);        
+
+            list.Add(m);
+        }
+        internal static List<BandInfo> GetAllBandInfos(int minIndex)
+        {
+            List<BandInfo> list = new List<BandInfo>();
+
+            SafeProcedure.ExecuteAndMapRecords(Database.GetDatabase(D4DDefine.DBInstanceName),
+               "dbo.BandInfo_GetAll",
+               delegate(IParameterSet parameters)
+               {
+                   parameters.AddWithValue("@MinIndex", minIndex);           
+               },
+               delegate(IRecord record)
+               {
+                   MapBandInfoList(record, list);
+               }              
+           );
+
+            return list;
+        }
+
+        #endregion
 
     }
 }
