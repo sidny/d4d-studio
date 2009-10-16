f62b038260cf5d9a41a07463ad6e9c90cb257bd8
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-10-16 12:42:06 +0000

    add upload Control

---
+++ branches/D4D/D4D.Web/admin/Upload/UploadTest.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+namespace D4D.Web.admin.Upload
+{
+    public partial class UploadTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write(FileUpload1.UploadHttpFilePath);
+        }
+
+       
+    }
+}
