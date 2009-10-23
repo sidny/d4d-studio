f4bb0dc8df7c76a8c61ba434609e283a22af5ae4
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-10-23 06:34:20 +0000

    add seach platform method

---
+++ branches/D4D/D4D.Platform/Providers/SearchProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using D4D.Platform.Domain;
+using D4D.Platform.Persistence;
+namespace D4D.Platform.Providers
+{
+    public class SearchProvider
+    {
+        #region instance
+        private static readonly SearchProvider instance = new SearchProvider();
+
+        internal static SearchProvider Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        #endregion
+
+        public List<SearchResult> GetPagedSearch(PagingContext pager,
+            string searchText, ObjectTypeDefine objectType)
+        {
+            if (string.IsNullOrEmpty(searchText)) return new List<SearchResult>();
+            return SearchDao.GetPagedSearch(pager, searchText, objectType);
+        }
+    }
+}
