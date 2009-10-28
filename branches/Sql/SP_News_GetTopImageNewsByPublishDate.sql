ece63a1a3c9c09fd25cc22c331171460b5cebcd9
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-10-28 07:41:20 +0000

    add get top image news sp

---
+++ branches/Sql/SP_News_GetTopImageNewsByPublishDate.sql
@@ -0,0 +1,73 @@
+USE [d4d]
+GO
+/****** Object:  StoredProcedure [dbo].[Tags_GetTopTag]    Script Date: 10/28/2009 14:56:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Create date: 2009-10-10
+-- Description:	Add One TagsHits
+-- =============================================
+CREATE PROCEDURE [dbo].[News_GetTopImageNewsByPublishDate]  
+ @RemarkType AS INT,
+ @MaxCount AS INT,
+ @STime DATETIME,
+  @ETime DATETIME
+AS
+BEGIN	
+	SET ROWCOUNT @MaxCount	
+	
+      IF (@RemarkType=1)--Video
+      BEGIN
+      	SELECT   
+		       NewsId ,
+               Title,
+			   Body,
+			   Preview,
+			   SImage,
+			   LImage,
+			   NewsType,
+			   Hits,
+			   PublishDate,
+			   AddUserId,
+			   AddDate,
+			  [Status],
+			  Remark
+		FROM dbo.newsinfo  WITH(NOLOCK)
+		WHERE  Remark='video' 
+		AND  [Status] =1
+		AND PublishDate>=@STime 
+		AND PublishDate<=@ETime
+		AND (SImage <> '' AND SIMAGE IS NOT NULL)
+		ORDER BY PublishDate DESC
+      END
+      ELSE
+      BEGIN
+		  SELECT   
+		       NewsId ,
+               Title,
+			   Body,
+			   Preview,
+			   SImage,
+			   LImage,
+			   NewsType,
+			   Hits,
+			   PublishDate,
+			   AddUserId,
+			   AddDate,
+			  [Status],
+			  Remark
+		FROM dbo.newsinfo  WITH(NOLOCK)
+		WHERE  Remark<>'video'
+		 AND  [Status] =1
+		  AND PublishDate>=@STime 
+		AND PublishDate<=@ETime
+		AND (SImage <> '' AND SIMAGE IS NOT NULL)
+		ORDER BY PublishDate DESC
+      END
+     
+     
+
+ 
+END
