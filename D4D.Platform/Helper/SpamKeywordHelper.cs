ff70d88594cb98ae8ff63bd5cfc15a6cf80c77ed
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-11-01 12:09:30 +0000

    add keyword provider method

---
+++ D4D.Platform/Helper/SpamKeywordHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace D4D.Platform.Helper
+{
+   public static class SpamKeywordHelper
+    {
+
+       public const string DEFAULT_REPLACESTRING = "*";
+  
+
+       public static string FilterContent(string content)
+       {
+           return FilterContent(content,DEFAULT_REPLACESTRING);
+       }
+
+       public static string FilterContent(string content, string replaceStr)
+       {
+           if (string.IsNullOrEmpty(replaceStr)) return content;
+
+           string resultStr = content;
+
+           List<string> spamWordList = D4DGateway.SpamKeywordProvider.GetSpamKeywords();
+
+           if (spamWordList != null && spamWordList.Count > 0)
+           {
+               foreach (string s in spamWordList)
+                   resultStr = resultStr.Replace(s, replaceStr);
+           }
+
+           return resultStr;
+       }
+
+    }
+}
