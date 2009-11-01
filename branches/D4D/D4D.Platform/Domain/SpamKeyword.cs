fe8db953a93a54dc65b2d8b4777e7e7690eacc4f
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-11-01 12:09:30 +0000

    add keyword provider method

---
+++ branches/D4D/D4D.Platform/Domain/SpamKeyword.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace D4D.Platform.Domain
+{
+    public class SpamKeyword : BaseD4DDomain
+    {
+        public SpamKeyword() { }
+        public SpamKeyword(int id)
+        {
+            Id = id;
+        }
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Keyword
+        {
+            get;
+            set;
+        }    
+
+        public int Status
+        {
+            get;
+            set;
+        }
+    }
+}
