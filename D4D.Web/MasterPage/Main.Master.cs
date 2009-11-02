cba33a3e0c6ab84d33e601e8f4873397f2977c12
duo.fan.yang <duo.fan.yang@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-11-02 16:59:34 +0000

    add Title & Description

--- D4D.Web/MasterPage/Main.Master.cs
+++ D4D.Web/MasterPage/Main.Master.cs
@@ -11,7 +11,39 @@ namespace D4D.Web.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            string[] path = Request.AppRelativeCurrentExecutionFilePath.Split('/');
+            string title = "";
+            if (path.Length > 3)
+            {
+                switch (path[2])
+                {
+                    case "news":
+                        title += "星闻 - ";
+                        break;
+                    case "video":
+                        title += "视频 - ";
+                        break;
+                    case "singer":
+                        title += "艺人 - ";
+                        break;
+                    case "search":
+                        title += "搜索 - ";
+                        break;
+                    case "photo":
+                        title += "图片 - ";
+                        break;
+                    case "music":
+                        title += "音乐 - ";
+                        break;
+                    case "calender":
+                        title += "星程 - ";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            title += "少城时代官方网站";
+            Page.Header.Title = title;
         }
     }
 }
