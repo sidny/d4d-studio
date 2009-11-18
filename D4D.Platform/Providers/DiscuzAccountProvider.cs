92b51157cdb497c261087d0db7bc2a0dcabbf737
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-11-18 08:23:30 +0000

    add Discuzaccount

---
+++ D4D.Platform/Providers/DiscuzAccountProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using D4D.Platform.Domain;
+using D4D.Platform.Persistence;
+namespace D4D.Platform.Providers
+{
+    public class DiscuzAccountProvider
+    {
+        #region instance
+        private static readonly DiscuzAccountProvider instance = new DiscuzAccountProvider();
+
+        internal static DiscuzAccountProvider Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        #endregion
+
+
+    }
+}
