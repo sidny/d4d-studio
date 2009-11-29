a7dd3123fc6ecfd119a90bbc76db1a8d63b3b5c9
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-11-29 09:23:30 +0000

    add jane.shop

---
+++ branches/D4D/JANE.Shop/JaneShopGateway.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JANE.Shop.Providers;
+namespace JANE.Shop
+{
+    public static class JaneShopGateway
+    {
+        public static JaneShopProvier JaneShopProvier
+        {
+            get
+            {
+                return JaneShopProvier.Instance;
+            }
+        }
+    }
+}
