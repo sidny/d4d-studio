4d23b75fdeb33feb7d41662fde3c8123c0a3a6e0
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-11-29 09:23:30 +0000

    add jane.shop

---
+++ branches/D4D/JANE.Shop/Domain/JaneDefine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JANE.Shop.Domain
+{
+
+
+    public class JaneDefine
+    {
+        public const string JaneBBSDBInstanceName = "janebbs";
+        public const string DBInstanceName = "jane";
+    }
+
+    /// <summary>
+    /// 发布状态
+    /// </summary>
+    public enum ShopStatus : int
+    {
+        None = 0,//未发布,下架？
+        Publish = 1,//发布 
+        ALL = 2
+    }
+
+}
---
+++ branches/D4D/JANE.Shop/Domain/ShopItem.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using D4D.Platform.Domain;
+
+namespace JANE.Shop.Domain
+{
+    public class ShopItem : BaseD4DDomain
+    {
+          public ShopItem() { }
+          public ShopItem(int id)
+        {
+            Id = id;
+        }
+         public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        public decimal Price
+        {
+            get;
+            set;
+        }
+
+
+        public DateTime PublishDate
+        {
+            get;
+            set;
+        }      
+     
+      
+        public string SImage
+        {
+            get;
+            set;
+        }
+
+        public string LImage
+        {
+            get;
+            set;
+        }
+
+
+        public PublishStatus Status
+        {
+            get;
+            set;
+        }
+    }
+}
