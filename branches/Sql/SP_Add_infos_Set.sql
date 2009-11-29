07d78964a709e7474c205722a4b6e1089a2a2b14
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-11-29 11:48:59 +0000

    附加信息表，给唱片的joyo和当当链接用，订单相关存储过程

---
+++ branches/Sql/SP_Add_infos_Set.sql
@@ -0,0 +1,51 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Create date: 2009-11-29
+-- =============================================
+CREATE PROCEDURE [dbo].[Add_infos_Set]
+   @objectid AS INT ,
+   @objecttype AS int,  
+   @info1 AS NVARCHAR(4000), 
+    @info2 AS NVARCHAR(4000), 
+	 @info3 AS NVARCHAR(4000), 
+	  @info4 AS NVARCHAR(4000)
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+	
+	UPDATE add_infos
+    SET objectid = @objectid,objecttype=@objecttype,info1= @info1,
+    info2=@info2,info3=@info3,info4=@info4
+    WHERE objectid =  @objectid AND objecttype=@objecttype
+	
+	IF @@ROWCOUNT = 0
+	BEGIN
+	INSERT INTO add_infos
+			   (objectid,
+			   objecttype,
+			   info1,
+			   info2,
+			  info3,
+			   info4				  
+			   )
+		 VALUES
+			   (@objectid,
+			   @objecttype,
+			   @info1,
+			   @info2,
+			  @info3,
+			   @info4	
+			 )
+		
+	END
+  
+ 
+    
+  
+	
+
+END
---
+++ branches/Sql/Table_add_infos.sql
@@ -0,0 +1,14 @@
+﻿if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[add_infos]') and OBJECTPROPERTY(id, N'IsUserTable') = 1)
+drop table [dbo].[add_infos]
+GO
+
+CREATE TABLE [dbo].[add_infos] (
+	[objectid] [int] NULL ,
+	[objecttype] [int] NULL ,
+	[info1] [nvarchar] (1000) COLLATE Chinese_PRC_CI_AS NULL ,
+	[info2] [nvarchar] (1000) COLLATE Chinese_PRC_CI_AS NULL ,
+	[info3] [nvarchar] (1000) COLLATE Chinese_PRC_CI_AS NULL ,
+	[info4] [nvarchar] (1000) COLLATE Chinese_PRC_CI_AS NULL 
+) ON [PRIMARY]
+GO
+
