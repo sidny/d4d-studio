9c35e42f15fee24565218191b7158e3923d10c1d
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-11-30 09:38:36 +0000

    增加购物清单表及存储过程

---
+++ branches/Sql/Jane/SP_Shop_tradelist_Deletebyorderid.sql
@@ -0,0 +1,17 @@
+﻿
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Create date: 2009-11-29
+-- =============================================
+CREATE PROCEDURE [dbo].[Shop_tradelist_Deletebyorderid]   
+   @orderid AS INT 
+AS
+BEGIN	
+	
+   DELETE    FROM Shop_tradelist
+    WHERE orderid =  @orderid
+ 
+END
---
+++ branches/Sql/Jane/SP_Shop_tradelist_Getbyorderid.sql
@@ -0,0 +1,22 @@
+﻿
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Create date: 2009-11-29
+-- =============================================
+CREATE PROCEDURE [dbo].[Shop_tradelist_Getbyorderid]   
+   @orderid AS INT 
+AS
+BEGIN	
+	
+    SELECT 
+               Id, 
+              orderid,
+			   itemid,
+			   itemcount
+    FROM Shop_tradelist With(nolock)
+    WHERE orderid =  @orderid
+ 
+END
