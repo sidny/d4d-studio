fa4c2ff26abb2a02246b1504ef1f89a2b964f2d0
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2009-11-30 09:38:36 +0000

    增加购物清单表及存储过程

---
+++ branches/Sql/Jane/SP_Shop_tradelist_Delete.sql
@@ -0,0 +1,17 @@
+﻿
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Create date: 2009-11-29
+-- =============================================
+CREATE PROCEDURE [dbo].[Shop_tradelist_Delete]   
+   @id AS INT 
+AS
+BEGIN	
+	
+   DELETE    FROM Shop_tradelist
+    WHERE id =  @id
+ 
+END
