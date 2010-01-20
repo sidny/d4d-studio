e3f997c1aab5a63d8f8002d43a72d76b745e6709
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2010-01-20 03:08:52 +0000

    地域的价格

---
+++ Jane/Table_shop_region.sql
@@ -0,0 +1,14 @@
+﻿if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[shop_region]') and OBJECTPROPERTY(id, N'IsUserTable') = 1)
+drop table [dbo].[shop_region]
+GO
+
+CREATE TABLE [dbo].[shop_region] (
+	[Id] [int] IDENTITY (1, 1) NOT NULL ,
+	[Name] [nvarchar] (50) COLLATE Chinese_PRC_CI_AS NULL ,
+	[ParentId] [int] NULL ,
+	[TransferPrice] [float] NULL ,
+	[TransferPrice1] [float] NULL ,
+	[TransferPrice2] [float] NULL 
+) ON [PRIMARY]
+GO
+
