b677a61a1d521f91718c5670166a0912f856e58f
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2010-01-20 03:08:52 +0000

    地域的价格

---
+++ branches/Sql/Jane/SP_Shop_region_Delete.sql
@@ -0,0 +1,18 @@
+﻿
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Create date: 2009-11-29
+-- =============================================
+CREATE PROCEDURE [dbo].[Shop_region_Delete]
+   @Id AS INT 
+AS
+BEGIN	
+
+   DELETE FROM  shop_region
+   WHERE Id = @Id
+   
+	
+END
---
+++ branches/Sql/Jane/SP_Shop_region_Set.sql
@@ -0,0 +1,44 @@
+﻿-- =============================================
+-- Create date: 2009-11-29
+-- =============================================
+CREATE PROCEDURE [dbo].[Shop_region_Set]
+   @id AS INT ,  
+   @Name AS NVARCHAR(50), 
+      @ParentId AS INT ,  
+   @TransferPrice AS float, 
+@TransferPrice1 AS float, 
+@TransferPrice2 AS float
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+  
+    IF (@id<=0)
+   BEGIN 
+	INSERT INTO shop_region
+			   (Name,
+			   ParentId,
+			   TransferPrice,
+			   TransferPrice1,
+			  TransferPrice2
+			   )
+		 VALUES
+			   (@Name,
+			   @ParentId,
+			   @TransferPrice,
+			   @TransferPrice1,
+			   @TransferPrice2
+			 )
+		SET @id = 	 @@IDENTITY 
+   END
+   ELSE
+   BEGIN
+    UPDATE shop_region
+    SET Name = @Name,ParentId=@ParentId,TransferPrice= @TransferPrice,
+    TransferPrice1=@TransferPrice1,TransferPrice2=@TransferPrice2
+    WHERE id =  @id
+   END
+	
+	RETURN @id 
+END
+GO
