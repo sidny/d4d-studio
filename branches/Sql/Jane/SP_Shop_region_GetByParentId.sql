ddc374f052f12d7293a6d0e1c5db3dccc6520f17
fengxinwei <fengxinwei@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2010-01-20 03:08:52 +0000

    地域的价格

---
+++ branches/Sql/Jane/SP_Shop_region_GetByParentId.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Create date: 2009-11-29
+-- =============================================
+CREATE PROCEDURE [dbo].[Shop_region_GetByParentId]   
+   @ParentId AS INT 
+AS
+BEGIN	
+	
+    SELECT 
+               Id, 
+               Name,
+			   ParentId,
+			   TransferPrice,
+			   TransferPrice1,
+			  TransferPrice2
+    FROM shop_region With(nolock)
+    WHERE ParentId =  @ParentId
+ 
+END
+
+GO
