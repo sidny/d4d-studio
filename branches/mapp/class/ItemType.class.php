5efe138327db834e840c9a28d7cb15f8c3e8c256
duo.fan.yang <duo.fan.yang@4213c7d4-9ba6-11de-bb5f-cdebcd2927f5>
2012-11-17 09:33:08 +0000

    init

--- branches/mapp/class/ItemType.class.php
+++ branches/mapp/class/ItemType.class.php
@@ -1,155 +1,21 @@
 <?php
+class ItemType extends Base {
+	protected static $table = "item_type";
 
-class ItemType {
-
-	static private $tablename = "itemtype";
-
-	
-	public function __construct() {
-	  
+	public static function search($keyword){
+		$condition[] = array('title', '%' . $keyword .'%', 'like');
+		return self::getList(array('condition' => $condition));
 	}
-	private static function Instance()
-	{
-		if (!isset(self::$msInstance)) {
-			self::$msInstance = new self;
-		}
+	public static function getList($params,$offset = null, $count = null ){
+		return Base::getList(self::$table, $params,$offset, $count) ;
 	}
-
-	/**
-	 * Returns all the rows from the table.
-	 *
-	 * Add authroization or any logical checks for secure access to your data 
-	 *
-	 * @return array
-	 */
-	static public  function getAllItem($parentid = null) {
-		$condition = array(
-			'1' => '1'
-		);
-		if(isset($parentid)){
-			$condition['parentid'] = $parentid;
-		}
-		$param = array(
-			'condition' => $condition,
-			'order'		=> 'id asc',
-		);
-		$result = tdb::select(self::$tablename,$param);
-		
-		return $result;
+	public static function add($params){
+		return Base::add(self::$table,$params);
 	}
-
-	/**
-	 * Returns the item corresponding to the value specified for the primary key.
-	 *
-	 * Add authorization or any logical checks for secure access to your data 
-	 *
-	 * 
-	 * @return stdClass
-	 */
-	public function getItemByID($itemID) {
-		$params = array(
-		 	'condition' =>array(
-				'id' => $itemID,
-			),
-		);
-		return tdb::select(self::$tablename,$params);
+	public static function del($params){
+		return  Base::del(self::$table,$params);
 	}
-
-	/**
-	 * Returns the item corresponding to the value specified for the primary key.
-	 *
-	 * Add authorization or any logical checks for secure access to your data 
-	 *
-	 * 
-	 * @return stdClass
-	 */
-	public function createItem($item) {
-		return tdb::insert(self::$tablename,$item);
+	public static function update($item){
+		return  Base::update(self::$table,$item);
 	}
-
-	/**
-	 * Updates the passed item in the table.
-	 *
-	 * Add authorization or any logical checks for secure access to your data 
-	 *
-	 * @param stdClass $item
-	 * @return void
-	 */
-	public function updateItem($item) {
-			if(isset($item['id'])){
-				$id = $item['id'];
-				unset($item['id']);
-
-				$params['set'] =$item;
-				$params['condition'] = array('id' => $id); 
-				return tdb::update(self::$tablename,$params);
-			}
-			return false;
-	}
-
-	/**
-	 * Deletes the item corresponding to the passed primary key value from 
-	 * the table.
-	 *
-	 * Add authorization or any logical checks for secure access to your data 
-	 *
-	 * 
-	 * @return void
-	 */
-	public function deleteItem($itemID) {
-		$condition = array(
-			'id' => $itemID
-		);
-		
-		return tdb::delete(self::$tablename,array('condition' => $condition));
-	}
-
-
-	/**
-	 * Returns the number of rows in the table.
-	 *
-	 * Add authorization or any logical checks for secure access to your data 
-	 *
-	 * 
-	 */
-	public function count($typeId = null) {
-		$condition = array(
-			'1' => '1'
-		);
-		if($typeId){
-			$condition['typeId'] = $typeId;
-		}
-		return tdb::count(self::$tablename,array('condition' => $condition));
-	}
-
-
-	/**
-	 * Returns $numItems rows starting from the $startIndex row from the 
-	 * table.
-	 *
-	 * Add authorization or any logical checks for secure access to your data 
-	 *
-	 * 
-	 * 
-	 * @return array
-	 */
-	public function getItem_paged($startIndex, $numItems,$typeId= null) {
-		$condition = array(
-				'1' => '1'
-		);
-		if($typeId){
-			$condition['typeId'] = $typeId;
-		}
-		$param = array(
-			'condition' => $condition,
-			'order' => 'id asc', 
-			'limit' => $numItems,
-			'offset' => $startIndex,
-		);
-		$result = tdb::select(self::$tablename,$param);
-
-		return $result;
-	}
-	
-	
 }
--- branches/mapp/webroot/index.php
+++ branches/mapp/webroot/index.php
@@ -1,22 +1,11 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../init.inc.php');
 Template::SetTitle('首页');
-$params =  array(
-	'condition' => array(
-		array('typeid',6)
-	),
-	//按修改时间排序
-	'order'	=> array(
-		array('itemtime','desc')
-	)
-);
-$usernames = Array(
-	'a' => 'zhaolujie',
-	'b' => 'wanghan',
-	'c' => 'likunming',
-	'd' => 'yezi'
-);
-$indexType = ContentList::getList($params);
-Template::assign('indexType',$indexType['list']);
-Template::assign('usernames',$usernames);
+if(isset($_REQUEST['type'])){
+	$options  = array(
+		'type' => (int)$_REQUEST['type'],	
+	);
+}
+$itemType  = ItemType::getList($options);
+Template::assign('itemType',$itemType);
 Template::display('index.tpl');
